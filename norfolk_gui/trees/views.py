7ae4f0ba4503b3143d18811b1cde177c50996976
j-miskill <jcmiskill@gmail.com>
2023-11-17 12:25:21 -0500

    added the django views moduel

--- norfolk_gui/trees/views.py
+++ norfolk_gui/trees/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
 
@@ -7,3 +9,6 @@ def home_view(request):
     template_name = "trees/home.html"
     context = {"hello": "hello world"}
     return render(request=request, context=context, template_name=template_name)
+
+
+class
