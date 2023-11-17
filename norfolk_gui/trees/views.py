edd56b3a15c4ee202f912417553d62517f25ba2b
j-miskill <jcmiskill@gmail.com>
2023-11-17 10:02:40 -0500

    working version of the "hello, world" template. Will make structural change later

--- norfolk_gui/trees/views.py
+++ norfolk_gui/trees/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 
 
 def home_view(request):
+    template_name = "trees/home.html"
     context = {"hello": "hello world"}
-    return render(request, context)
+    return render(request=request, context=context, template_name=template_name)
