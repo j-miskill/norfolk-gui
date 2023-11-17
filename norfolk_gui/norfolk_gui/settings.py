332c321a8a9139dd0e51fa9fe3e9f38f291ab3eb
j-miskill <jcmiskill@gmail.com>
2023-11-17 13:09:50 -0500

    added functionality for getting the API key from the environment. I will add this to the environment variables page.

--- norfolk_gui/norfolk_gui/settings.py
+++ norfolk_gui/norfolk_gui/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MAPS_KEY = os.environ.get("MAPS_KEY")
