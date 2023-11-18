b46e10e33e0ffd4b1bfbb24fd019770aee1e209f
j-miskill <jcmiskill@gmail.com>
2023-11-18 15:55:31 -0500

    finally got the map working. Will try to figure out the Norfolk Socrata API next!

--- norfolk_gui/trees/static/trees/map.js
+++ norfolk_gui/trees/static/trees/map.js
@@ -3,9 +3,9 @@ let map;
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     map = new Map(document.getElementById("map"), {
-        center: { lat: 36.88809, lng: 76.28566},
-        zoom: 8,
+        center: { lat: 36.88809, lng: -76.28566},
+        zoom: 15,
     });
 }
 
-window.initMap = initMap;
+initMap();
