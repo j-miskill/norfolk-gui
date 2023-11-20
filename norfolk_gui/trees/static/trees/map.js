027d37bd02015f4f21930761120ce89a2a8c6c1f
j-miskill <jcmiskill@gmail.com>
2023-11-19 22:36:03 -0500

    I should have commit more often, but here is a working version of the significant trees plotted using the google maps API!

--- norfolk_gui/trees/static/trees/map.js
+++ norfolk_gui/trees/static/trees/map.js
@@ -1,11 +1,24 @@
 
 let map;
 async function initMap() {
+    let lat_long_data = document.getElementById("data").value;
+
     const { Map } = await google.maps.importLibrary("maps");
     map = new Map(document.getElementById("map"), {
-        center: { lat: 36.88809, lng: -76.28566},
+        center: { lat: 36.8477, lng: -76.2951},
         zoom: 15,
     });
+
+    let parsed_data = JSON.parse(lat_long_data);
+    //console.log(parsed_data);
+    for (const tree in parsed_data) {
+        //console.log(tree);
+        const marker = new google.maps.Marker({
+                position: {lat: parseFloat(parsed_data[tree].latitude), lng: parseFloat(parsed_data[tree].longitude)},
+                map: map,
+            });
+    }
+
 }
 
 initMap();
--- norfolk_gui/trees/views.py
+++ norfolk_gui/trees/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views import View
 from sodapy import Socrata
 from pprint import pprint
+import json
 
 
 # Create your views here.
@@ -19,18 +20,20 @@ class TreeView(View):
     template_name = "trees/map.html"
     key = os.environ.get("MAPS_KEY")
     tree_key = os.environ.get("TREE_KEY")
+
     def get(self, request):
         context = {"key": self.key}
-        tree_data = self.get_tree_data(self.tree_key)
+        tree_data = self.get_tree_data()
         lat_long_data = self.clean_tree_data(tree_data)
+        context["lat_long_data"] = json.dumps(lat_long_data)
         return render(request=request, template_name=self.template_name, context=context)
 
-    def get_tree_data(self, tree_key: str):
+    def get_tree_data(self):
         data = {}
         try:
-            client = Socrata("data.norfolk.gov", app_token=tree_key)
+            client = Socrata("data.norfolk.gov", app_token=self.tree_key)
             data = client.get("jz6u-9g3c", limit=471)
-            pprint(data[0])
+            # pprint(data[0])
         except:
             raise Exception("Something went wrong")
         return data
@@ -44,6 +47,7 @@ class TreeView(View):
             lat_long_data[name] = {}
             lat_long_data[name]["latitude"] = lat
             lat_long_data[name]["longitude"] = long
+
         return lat_long_data
 
 
