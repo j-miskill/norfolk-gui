9367f696ad37adabf4fa776bc4ba8e09c593dd0a
j-miskill <jcmiskill@gmail.com>
2023-11-19 21:53:09 -0500

    almost done with the request for the norfolk data script

--- norfolk_gui/trees/views.py
+++ norfolk_gui/trees/views.py
@@ -2,6 +2,8 @@ import os
 
 from django.shortcuts import render
 from django.views import View
+from sodapy import Socrata
+from pprint import pprint
 
 
 # Create your views here.
@@ -9,13 +11,42 @@ from django.views import View
 
 def home_view(request):
     template_name = "trees/home.html"
-    context = {"hello": "hello world"}
+    context = {}
     return render(request=request, context=context, template_name=template_name)
 
 
 class TreeView(View):
     template_name = "trees/map.html"
     key = os.environ.get("MAPS_KEY")
+    tree_key = os.environ.get("TREE_KEY")
     def get(self, request):
         context = {"key": self.key}
+        tree_data = self.get_tree_data(self.tree_key)
+        lat_long_data = self.clean_tree_data(tree_data)
         return render(request=request, template_name=self.template_name, context=context)
+
+    def get_tree_data(self, tree_key: str):
+        data = {}
+        try:
+            client = Socrata("data.norfolk.gov", app_token=tree_key)
+            data = client.get("jz6u-9g3c", limit=471)
+            pprint(data[0])
+        except:
+            raise Exception("Something went wrong")
+        return data
+
+    def clean_tree_data(self, tree_data: dict):
+        lat_long_data = {}
+        for tree in tree_data:
+            name = tree["id"]
+            lat = tree["latitude"]
+            long = tree["longitude"]
+            lat_long_data[name] = {}
+            lat_long_data[name]["latitude"] = lat
+            lat_long_data[name]["longitude"] = long
+        return lat_long_data
+
+
+
+
+
